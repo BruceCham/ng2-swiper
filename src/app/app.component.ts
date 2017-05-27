d70f8968a2613160b6e478e2f74b5ad47f51977b
brucecham <brucecham@qq.com>
2017-05-27 17:47:11 +0800

    ng2

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+declare let Swiper:any
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent {
+  constructor(
+    ) { }
+
+  ngAfterViewInit(){
+    new Swiper('.swiper-container',{
+        pagination: '.swiper-pagination',
+        paginationClickable: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        autoplay: 1500,
+        spaceBetween: 30
+    });
+  }
+}
